4f2e5129ecec43e2ba44529e1498440528e04e42
gulnur <abdrashitovagulnur05@gmail.com>
2024-07-26 18:05:18 +0300

    save

---
+++ script.js
@@ -0,0 +1,67 @@
+
+// zadacha-1
+
+// function createStudent(firstName, lastName, age, university, faculty, gpa) {
+//   const user = {
+//     firstName: "Nooruz" +" Abdykalykov",
+//     age: 24,
+//     education: {
+//       universiti: "ALATOO",
+//       faculti: "CS",
+//       gpa: 87,
+//     },
+//   };
+//   return user;
+// }
+
+// const res = createStudent("Nooruz", "Abdykaly", "24", "ALATOO", "CS", "87");
+// console.log(res);
+
+// zadacha-2
+
+//  const object={
+//   name1 :"gulnur",
+//  }
+
+//  function deleteKey(obj,key ){
+//      if("deletekey" in object){
+//       console.log(`${Key},ест такой ключ `)
+//      }else{
+//       console.log(`${key},нет такой ключ`);
+//      }
+//      return name1;
+//  }
+//  delete object.name1
+
+//  deleteKey(object,"name");
+
+// zadacha-3
+
+// let user = {
+//   name: "gulnur",
+//   age: 19,
+//   email: "gulnur@gmail.com",
+//   password: "gulnur123",
+// };
+// const sum = 5;
+// const user2 = prompt("Ведите ваше email");
+// if (user2 !== user.email) {
+//   alert("такой email нет");
+// } else if (user2 === user.email) {
+//   const password = prompt("Пароль");
+//   if (password === user.password) {
+//     alert("Tyyra ");
+//     const user3 = prompt("2+3=?");
+//     if (parseInt(user3) === sum) {
+//       user.sum = 5;
+//       alert(`премия алдым = ${user.sum}`);
+//     } else {
+//       alert("не правелный ответ");
+//     }
+//   } else {
+//     alert("Пароль туура emes");
+//   }
+// }
+
+
+
